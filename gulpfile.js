ba9ceed7261571a2c50fefbf5be79f6fb70b839f
Hristo Ivanov <hristoivanov@Hristos-MacBook-Air.local>
2018-02-18 19:45:23 +0200

    Structure

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// Dependencies
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const autoprefixer = require('autoprefixer');
+
+// Configurations
+const buildFiles = [
+    'eve.scss',
+    'sass/**/*.scss'
+];
+const buildDestination = 'build';
+const buildFileName = 'eve';
+
+// Tasks
+gulp.task('build-eve', () => {
+    return gulp.src(buildFiles)
+        .pipe(sass())
+        .pipe(rename({
+            basename: buildFileName
+        }))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest(buildDestination));
+});
+
+gulp.task('build-eve-min', () => {
+    return gulp.src(buildFiles)
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(rename({
+            basename: buildFileName,
+            suffix: '.min'
+        }))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest(buildDestination));
+});
+
+gulp.task('build', ['build-eve', 'build-eve-min']);
+
+gulp.task('watch', () => {
+    gulp.watch(buildFiles, ['build']);
+});
