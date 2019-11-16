c780a0dfc70a039445b0cda49b09e976f3c832e2
hpivanov <hristo.penev.ivanov@gmail.com>
2019-11-16 23:34:20 +0200

    add builder.json

--- gulpfile.js
+++ gulpfile.js
@@ -8,115 +8,85 @@ var gulpif = require('gulp-if');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-// Configurations
-var config = {
-    eve: {
-        src: ['src/**/*.scss'],
-        dest: ['dist', 'docs/dist'],
-        out: 'eve'
-    },
-    docs: {
-        js: {
-            src: ['docs/documentation/scripts/**/*.js'],
-            dest: 'docs/documentation/dist',
-            out: 'eve-docs'
-        },
-        css: {
-            src: ['docs/documentation/styles/**/*.scss'],
-            dest: 'docs/documentation/dist',
-            out: 'eve-docs'
-        }
-    },
-    site: {
-        js: {
-            src: ['docs/website/scripts/**/*.js'],
-            dest: 'docs/website/dist',
-            out: 'eve-website'
-        },
-        css: {
-            src: ['docs/website/styles/**/*.scss'],
-            dest: 'docs/website/dist',
-            out: 'eve-website'
-        }
-    }
-};
+// Builder
+var builder = require('./builder.json');
 
 // Eve tasks
 gulp.task('build-eve', function () {
-    return gulp.src(config.eve.src)
+    return gulp.src(builder.eve.src)
         .pipe(gulpif(!process.env.TRAVIS, plumber()))
         .pipe(sass.sync())
         .pipe(postcss())
         .pipe(rename({
-            basename: config.eve.out
+            basename: builder.eve.out
         }))
-        .pipe(gulp.dest(config.eve.dest[0]))
-        .pipe(gulp.dest(config.eve.dest[1]));
+        .pipe(gulp.dest(builder.eve.dest[0]))
+        .pipe(gulp.dest(builder.eve.dest[1]));
 });
 
 gulp.task('build-eve-min', function () {
-    return gulp.src(config.eve.src)
+    return gulp.src(builder.eve.src)
         .pipe(gulpif(!process.env.TRAVIS, plumber()))
         .pipe(sass.sync({
             outputStyle: 'compressed'
         }))
         .pipe(postcss())
         .pipe(rename({
-            basename: config.eve.out,
+            basename: builder.eve.out,
             suffix: '.min'
         }))
-        .pipe(gulp.dest(config.eve.dest[0]))
-        .pipe(gulp.dest(config.eve.dest[1]));
+        .pipe(gulp.dest(builder.eve.dest[0]))
+        .pipe(gulp.dest(builder.eve.dest[1]));
 });
 
 gulp.task('build', ['build-eve', 'build-eve-min']);
 
 // Docs tasks
 gulp.task('build-docs-js', function () {
-    return gulp.src(config.docs.js.src)
+    return gulp.src(builder.docs.js.src)
         .pipe(gulpif(!process.env.TRAVIS, plumber()))
         .pipe(uglify())
-        .pipe(concat(config.docs.js.out + '.min.js'))
-        .pipe(gulp.dest(config.docs.js.dest))
+        .pipe(concat(builder.docs.js.out + '.min.js'))
+        .pipe(gulp.dest(builder.docs.js.dest))
 });
 
 gulp.task('build-docs-css', function () {
-    return gulp.src(config.docs.css.src)
+    return gulp.src(builder.docs.css.src)
         .pipe(gulpif(!process.env.TRAVIS, plumber()))
         .pipe(sass.sync({
             outputStyle: 'compressed'
         }))
         .pipe(postcss())
         .pipe(rename({
-            basename: config.docs.css.out,
+            basename: builder.docs.css.out,
             suffix: '.min'
         }))
-        .pipe(gulp.dest(config.docs.css.dest));
+        .pipe(gulp.dest(builder.docs.css.dest));
 });
 
 gulp.task('build-docs', ['build-docs-js', 'build-docs-css']);
 
 // Site tasks
 gulp.task('build-site-js', function () {
-    return gulp.src(config.site.js.src)
+    return gulp.src(builder.site.js.src)
         .pipe(gulpif(!process.env.TRAVIS, plumber()))
         .pipe(uglify())
-        .pipe(concat(config.site.js.out + '.min.js'))
-        .pipe(gulp.dest(config.site.js.dest))
+        .pipe(concat(builder.site.js.out + '.min.js'))
+        .pipe(gulp.dest(builder.site.js.dest))
 });
 
 gulp.task('build-site-css', function () {
-    return gulp.src(config.site.css.src)
+    return gulp.src(builder.site.css.src)
         .pipe(gulpif(!process.env.TRAVIS, plumber()))
         .pipe(sass.sync({
             outputStyle: 'compressed'
         }))
         .pipe(postcss())
         .pipe(rename({
-            basename: config.site.css.out,
+            basename: builder.site.css.out,
             suffix: '.min'
         }))
-        .pipe(gulp.dest(config.site.css.dest));
+        .pipe(gulp.dest(builder.site.css.dest));
 });
 
 gulp.task('build-site', ['build-site-js', 'build-site-css']);
@@ -124,15 +94,15 @@ gulp.task('build-site', ['build-site-js', 'build-site-css']);
 // Watchers
 gulp.task('watch', ['build', 'build-docs', 'build-site'], function () {
     // Eve watcher
-    gulp.watch(config.eve.src, ['build']);
+    gulp.watch(builder.eve.src, ['build']);
 
     // Docs watchers
-    gulp.watch(config.docs.js.src, ['build-docs-js']);
-    gulp.watch(config.docs.css.src, ['build-docs-css']);
+    gulp.watch(builder.docs.js.src, ['build-docs-js']);
+    gulp.watch(builder.docs.css.src, ['build-docs-css']);
 
     // Site watchers
-    gulp.watch(config.site.js.src, ['build-site-js']);
-    gulp.watch(config.site.css.src, ['build-site-css']);
+    gulp.watch(builder.site.js.src, ['build-site-js']);
+    gulp.watch(builder.site.css.src, ['build-site-css']);
 });
 
 // Build all
